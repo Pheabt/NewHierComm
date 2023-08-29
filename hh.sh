09cc9a9d2c7ff716a45423ddb63bac5a7cc0af37
Phoenix <31894252+pheabt@users.noreply.github.com>
2023-08-30 00:35:00 +0800

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -2,10 +2,21 @@
 #SBATCH --job-name=my_job
 #SBATCH --output=output.log
 #SBATCH --error=error.log
-#SBATCH --partition=partition_name
+
+#SBATCH --partition=cpu_partition
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=3
+#SBATCH --cpus-per-task=6
+
+source /home/iotsc_g4/app/miniconda3/bin/activate tie
+
 
 cd /home/iotsc_g4/aaa/NewHierComm
-conda activate tie
-python main.py --agent ac_att --att_head 2
+
+
+srun  python main.py --agent ac_att  --att_head 1  --seed 6666 &
+srun  python main.py --agent ac_att  --att_head 2  --seed 6666 &
+srun  python main.py --agent ac_att  --att_head 3  --seed 6666 &
+srun  python main.py --agent ac_att  --att_head 4  --seed 6666 &
+
+wait
